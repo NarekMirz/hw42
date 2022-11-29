1a2e58b99641ffdf546d13f784747da222afca69
Narek <mirzakhanyan99@list.ru>
2022-11-29 18:18:55 +0300

    Готовое

---
+++ dz25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+int[] myArray = new int[8];
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    System.Console.WriteLine($"Введите элемент массива под индексом {i} ");
+    myArray[i] = int.Parse(Console.ReadLine() ?? "");
+}
+
+System.Console.WriteLine("Вывод массива: ");
+
+for (int i = 0 ; i < myArray.Length; i++)
+{
+    System.Console.Write(" " + myArray[i]);
+}
+
+Console.ReadLine();
