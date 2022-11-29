982e59e5a57691b247451ae83a9ac656850e6ea9
Narek <mirzakhanyan99@list.ru>
2022-11-29 18:06:59 +0300

    Готовое

---
+++ homework31/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+System.Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+int SumOfNum(int number)
+{
+    if(number < 10)
+        return number;
+    return number % 10 + SumOfNum(number/10);    
+}
+int result = SumOfNum(number);
+System.Console.WriteLine($"Ваше число {number}, сумма цифр вашего числа --> {result}");
+Console.ReadLine();
